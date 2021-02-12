460d70781a61a585a394b7b588f8a5283cc2f804
Dimitry <glasgo-seven@users.noreply.github.com>
2021-02-12 22:29:19 +0300

    from the start"

--- test/form/form.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-function login() {
-	if (document.getElementById('login').value == "admin" && document.getElementById('password').value == "admin")
-		return (window.location.href = "../menu/menu.html");
-	else
-		alert("Wrong login or password!");
-}
--- test/js/js.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-document.body.style.backgroundColor = "red";
