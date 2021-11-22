080505ea1769fbd7904d8635caa82500ec1f156c
glasgo-seven <glasgoseven@gmail.com>
2021-11-22 08:10:19 +0300

    new loaders & footer

---
+++ genshin.io/data.js
@@ -0,0 +1,58 @@
+var eventData_active = [
+	
+];
+
+var eventData_expired = [
+	[
+		"Браузерное событие «Сто тысяч почему от Паймон»",
+		"https://webstatic-sea.mihoyo.com/ys/event/answer-question/index.html?activity_no=b483abab3f704039&mhy_auth_required=true&mhy_presentation_style=fullscreen&utm_source=hoyolab&utm_medium=post"
+	],
+	[
+		"Браузерное событие «Пальчики оближешь! Барбекю под звёздами»",
+		"https://webstatic-sea.mihoyo.com/ys/event/e20211101fish/index.html?game_biz=hk4e_global&mhy_presentation_style=no_header&mhy_auth_required=true&mhy_landscape=true"
+	],
+	[
+		"Браузерное событие «Дневник исследований Элой»",
+		"https://mhy.link/2a68KHA6"
+	],
+	[
+		"Браузерное событие «Путешествие воспоминаний»",
+		"https://webstatic-sea.mihoyo.com/ys/event/e20210928review"
+	],
+	[
+		"Браузерное событие «Идём вместе»",
+		"https://mhy.link/857kIJA6"
+	],
+	[
+		"Браузерное событие «Послание времён»",
+		"https://webstatic-sea.mihoyo.com/ys/event/e20210930card/index.html?mhy_presentation_style=fullscreen&mhy_auth_required=true&game_biz=hk4e_global"
+	]
+];
+
+var galeryData = [
+	[
+		"Элементальные реакции",
+		"elemental-relations.jpg"
+	],
+	[
+		"Элементальное сопротивление",
+		"enemy-elemental-res.jpg"
+	],
+	[
+		"Система примогемов",
+		"primogems.jpg"
+	],
+	[
+		"Особые блюда персонажей",
+		"character-special-dishes.jpg"
+	],
+	[
+		"Материалы возвышения персонажей",
+		"character-ascension-materials.jpg"
+	],
+	[
+		"Расписание наград подземелий",
+		"dungeon-timeset.jpg"
+	],
+	
+];
