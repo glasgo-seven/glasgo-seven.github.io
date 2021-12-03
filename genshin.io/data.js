92a4938e990885def0e28aa2052c36c4661013b1
glasgo-seven <glasgoseven@gmail.com>
2021-12-03 13:50:57 +0300

    update db

--- genshin.io/data.js
+++ genshin.io/data.js
@@ -1,19 +1,19 @@
-var updateDate = "29.11.2021";
+var updateDate = "03.12.2021";
 
 var bannerData_active = [
 	[
 		"Молитва «Таинственные секреты» - «Принц мела» Альбедо (Гео)!",
-		"[24.11.2021 06:00 - 14.12.2021 19:59 (UTC+3)]",
+		"24.11.2021 06:00 - 14.12.2021 19:59 (UTC+3)",
 		"https://www.hoyolab.com/article/1432611"
 	],
 	[
 		"Молитва «Рождённая морской волной» - «Танец морской пены» Эола (Крио)!",
-		"[24.11.2021 06:00 - 14.12.2021 19:59 (UTC+3)]",
+		"24.11.2021 06:00 - 14.12.2021 19:59 (UTC+3)",
 		"https://www.hoyolab.com/article/1432612"
 	],
 	[
 		"Молитва «Воплощение божества» - Клятва свободы (Одноручное) и Песнь разбитых сосен (Двуручное)!",
-		"[24.11.2021 06:00 - 14.12.2021 19:59 (UTC+3)]",
+		"24.11.2021 06:00 - 14.12.2021 19:59 (UTC+3)",
 		"https://www.hoyolab.com/article/1432613"
 	]
 ];
@@ -25,15 +25,23 @@ var bannerData_expired = [
 var eventData_active = [
 	[
 		"Игровое событие «Белая пыль и снежная тень»",
-		"[25.11.2021 13:00 - 13.12.2021 05:59 (UTC+3)]",
+		"25.11.2021 13:00 - 13.12.2021 05:59 (UTC+3)",
 		"https://upload-os-bbs.hoyolab.com/upload/2021/11/19/1015537/863121d5e467dc1ffe13fab47bdf03b5_4468233710133244469.jpg?x-oss-process=image/resize,s_740/quality,q_80/auto-orient,0/interlace,1/format,jpg"
 	],
 	[
-		"[ОЖИДАЕТСЯ] Игровое событие «Дело Бантан Санго: Пёс-воин»",
-		"[07.12.2021 12:00 - 20.12.2021 05:59 (UTC+3)]",
-		"https://upload-os-bbs.hoyolab.com/upload/2021/11/19/1015537/84a1109146344cc3ba053eea11d52630_6775037132294758737.jpg?x-oss-process=image/resize,s_740/quality,q_80/auto-orient,0/interlace,1/format,jpg"
+		"Браузерное событие «Альбом путешествий»",
+		"03.12.2021 13:00 - 13.12.2021 05:59 (UTC+3)",
+		"https://webstatic-sea.mihoyo.com/ys/event/e20211203-album/index.html?game_biz=hk4e_global&mhy_presentation_style=fullscreen&mhy_auth_required=true&mhy_landscape=true&utm_source=hoyolab&utm_medium=post"
 	]
 ];
+
+var eventData_awaiting = [
+	[
+		"Игровое событие «Дело Бантан Санго: Пёс-воин»",
+		"07.12.2021 12:00 - 20.12.2021 05:59 (UTC+3)",
+		"https://upload-os-bbs.hoyolab.com/upload/2021/11/19/1015537/84a1109146344cc3ba053eea11d52630_6775037132294758737.jpg?x-oss-process=image/resize,s_740/quality,q_80/auto-orient,0/interlace,1/format,jpg"
+	]
+]
 
 var eventData_expired = [
 	[
